49f3e549d82fc93f535dc49e9e722a72c9e39244
wanglei <wanglei@asiainfo.com>
2018-12-05 10:06:07 +0800

    swagger得到返回值的配置，修改端口号

--- rookieparent/rookie-web/src/main/java/com/wanglei/rookieweb/SwaggerConfig.java
+++ rookieparent/rookie-web/src/main/java/com/wanglei/rookieweb/SwaggerConfig.java
@@ -7,6 +7,7 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -19,12 +20,12 @@ public class SwaggerConfig {
     @Bean
     public Docket createRestApi() {
         return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select()
-                .apis(RequestHandlerSelectors.basePackage("com")).paths(PathSelectors.any()).build();
+                .apis(RequestHandlerSelectors.basePackage("com.wanglei")).paths(PathSelectors.any()).build();
     }
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder().title("REST API文档").description("使用Swagger UI构建REST风格的可视化文档")
-                .termsOfServiceUrl("http://www.baidu.com").contact("wanglei@asiainfo.com")
+                .termsOfServiceUrl("http://localhost/swagger-ui.html").contact(new Contact("wanglei","http://localhost/swagger-ui.html","wanglei@asiaindo.com"))
                 .version("1.0.0-SNAPSHOT").build();
     }
 
